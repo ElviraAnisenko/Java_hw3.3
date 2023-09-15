d0d5dd5ffb1f9643b27aec3ea99c6384b641742e
Anisenko Elvira <elvira.anisenko@yandex.ru>
2023-09-15 14:39:13 +0300

    Add task

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,2 +1,10 @@
 public class CreditPaymentService {
+    public int calculate(int creditPeriodInMonths, int amount, double interestRate) {
+        double annuityRatio;
+        annuityRatio = interestRate / 12 / 100;
+        int payment;
+        payment = (int) ((int) amount * annuityRatio * Math.pow((1 + annuityRatio), creditPeriodInMonths) / (Math.pow((1 + annuityRatio), creditPeriodInMonths) - 1));
+        return payment;
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
 public class Main {
+    public static void main(String[] args) {
+        CreditPaymentService service = new CreditPaymentService();
+
+        System.out.println();
+        System.out.println("Сумма кредита  1 000 000 руб., срок кредита 1 год, процентная ставка 9,99%");
+        System.out.println("Ежемесячный платеж " + service.calculate(12, 1000000, 9.99) + " руб.");
+        System.out.println();
+        System.out.println("Сумма кредита  1 000 000 руб., срок кредита 2 года, процентная ставка 9,99%");
+        System.out.println("Ежемесячный платеж " + service.calculate(24, 1000000, 9.99) + " руб.");
+        System.out.println();
+        System.out.println("Сумма кредита  1 000 000 руб., срок кредита 3 года, процентная ставка 9,99%");
+        System.out.println("Ежемесячный платеж " + service.calculate(36, 1000000, 9.99) + " руб.");
+
+    }
 }
